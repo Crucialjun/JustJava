fb4bebd631e6e2298c0606f8cc7175155aafcf49
crucialjun <nich.otieno@gmail.com>
2018-06-11 10:44:05 +0300

    updating a variable

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -32,7 +32,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the +(plus) button is clicked
      */
     public void increment(View view) {
-        int quantity = 3;
+        int quantity = 2;
+        quantity = 3;
         display(quantity);
     }
 
